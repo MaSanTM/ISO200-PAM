b3cb0e80397f7c3eeed159c82bc3e92bc550f91a
Mateus Santos <mateus.h.santos@outlook.com.br>
2021-03-19 10:20:05 -0300

    Create userLogin.c

---
+++ userLogin.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+int main() 
+{
+    char userName[10]; //username
+    char passWord[10]; //password
+    
+    printf("\nType your username:\n");
+    scanf("%s", &userName);
+    
+    printf("\nEnter your password:\n");
+    scanf("%s", &password);
+    
+    if(strcmp(userName, "user")==0){
+        if(strcmp(passWord, "246")==0){
+            printf("\nLogin succesfull!\n")
+        }else{
+            printf("\nWrong password!\n"); //'ifs' gone wrong...
+        }
+    }
+    
+    return 0;
+}
