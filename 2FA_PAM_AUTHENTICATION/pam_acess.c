86ec453c8eca6aa8ba20ab9dc4aa2f3fd9dcf590
Mateus Santos <mateus.h.santos@outlook.com.br>
2021-05-27 19:09:59 -0300

    Add files via upload

---
+++ 2FA_PAM_AUTHENTICATION/pam_acess.c
@@ -0,0 +1,45 @@
+﻿/*
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   * AUTHOR: SANTOS, Mateus Henrique                        *
+   * SEE ME IN: linkedin.com/in/mateus-h-santos             *
+   * COPYRIGHT (C) Mateus Henrique dos Santos, 2021         *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+#include <dirent.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+#include <pwd.h>
+#include <security/pam_modules.h>
+#include <security/pam_ext.h>
+
+#define _GNU_SOURCE 
+    
+
+PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) 
+{
+	return PAM_SUCCESS;
+}
+PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) 
+{
+      FILE *g= fopen("/etc/pam.d/pam.acesso/acesso.serial", "r");
+   
+    if (!g)
+      {
+        perror("\nAccess denied!\n
+        See /etc/pam.d/pam.access/serial.access.");
+		sleep(2);
+		return PAM_AUTH_ERR;
+		
+      }
+   else
+	   return PAM_SUCCESS;
+	   
+       fclose(g);    
+}
