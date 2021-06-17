4e66f433610b763b6d59d0518d6fd9747c38bd9b
Mateus Santos <mateus.h.santos@outlook.com.br>
2021-06-17 20:33:11 -0300

    Update "src" folder

--- v.10/criar_arquivos.c
+++ v.10/criar_arquivos.c
@@ -203,6 +203,7 @@ int main(int argc, char *argv[]){
     puts("\n 1 - Make sure that the flash drive is connected and installed;");
     puts("\n 2 - Make sure thath only one flash drive is connected in your machine;");
     puts("\n 3 - If any USB device be detected, the program will be close.");
+    
     system("read -p \"\nPress Enter to continue...\" foo");
     system("clear"); 
 
